bca96ece6aefe63f4fdd73b753f17fcfb43dd5ee
pltduchoang <plt.duchoang@gmail.com>
2024-01-23 17:38:56 -0700

    update

--- myportfolio/app/src/components/Menu.js
+++ myportfolio/app/src/components/Menu.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useTheme } from '../utils/ThemeContext';
 
-const sections = ['bio','education','skills', 'projects'];
+const sections = ['bio','education' ,'achievements', 'skills', 'projects'];
 
 export default function Menu() {
     const [activeSection, setActiveSection] = useState('');
